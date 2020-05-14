a366a6e720b55dfbe101d2c3ba25518cf8bc00b8
Victor Williams <victor.williams1030@gmail.com>
2020-05-14 15:36:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,13 +2,8 @@ def my_hash
   your_hash = {"victor" => "awesome"}
 end
 
-
 def shipping_manifest
- the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2,
-                 
-                  "oil paintings" => 3
-
-
+ the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
