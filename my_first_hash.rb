8f78a796f650a43c0e15d32fa65260d5aeb53594
Victor Williams <victor.williams1030@gmail.com>
2020-05-14 15:42:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,21 +7,13 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3, "muskets" => 2, "gun powder" => 4}
-  # add 2 muskets to the shipping_manifest hash below
-
-  # add 4 gun powder to the shipping_manifest hash below
   
   shipping_manifest
-  # return the shipping_manifest hash below
 
 end
